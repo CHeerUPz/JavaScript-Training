4b3e0208d9504ed553ae5dd2ac9ddf23a1565c2c
Bernardo <52474526+CHeerUPz@users.noreply.github.com>
2019-12-01 22:02:23 +0000

    Update Caesars Cypher.js

--- Caesars Cypher.js
+++ Caesars Cypher.js
@@ -3,11 +3,10 @@ function rot13(str) {
   let abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   let cypher = ['N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M'];
   let strS = str.split(' ');                                
-  let strFinal = [];
+  let strFinal = '';
   
   for (let i = 0; i < strS.length; i++) {
     let strD = strS[i].split('');       
-    
     for (let j = 0; j < strD.length; j++) {
       for (let k = 0; k < abc.length; k++) {
         if (strD[j] == abc[k]) {
@@ -15,11 +14,10 @@ function rot13(str) {
         }
       }
     } 
-    strFinal = strFinal.join('');
+    strFinal += ' ';
   }
-  strFinal = strFinal.join(' ');                              
-  console.log('end: ' + strFinal);
-
+  console.log(strFinal);
+  
   return strFinal;
 }
 
