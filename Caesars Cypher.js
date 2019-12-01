88e609a056f1d68596f5a76f210cfae8b56ec005
Bernardo <52474526+CHeerUPz@users.noreply.github.com>
2019-12-01 21:52:19 +0000

    Update Caesars Cypher.js

--- Caesars Cypher.js
+++ Caesars Cypher.js
@@ -3,21 +3,26 @@ function rot13(str) {
   let abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   let cypher = ['N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M'];
   let strS = str.split(' ');                                
-  let strFinal = [[]];
+  let strFinal = [];
+  console.log('strSplit: ' + strS);
   
   for (let i = 0; i < strS.length; i++) {
-    let strD = strS[i].split('');                            
+    let strD = strS[i].split('');       
+    console.log('strDivided: ' + strD);
+    
     for (let j = 0; j < strD.length; j++) {
       for (let k = 0; k < abc.length; k++) {
         if (strD[j] == abc[k]) {
-          strFinal[i].push(cypher[k]);                        
+          strFinal.push(cypher[k]);
+          console.log('for cicle: ' + strFinal);
         }
       }
     } 
-    strFinal[i] = strFinal[i].join('');                       
+    strFinal = strFinal.join('');     
+    console.log('almost: ' + strFinal);
   }
   strFinal = strFinal.join(' ');                              
-  console.log(strFinal);
+  console.log('end: ' + strFinal);
 
   return strFinal;
 }
